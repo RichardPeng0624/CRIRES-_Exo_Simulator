c6f21e54769fc46921cf1a657c5bffec7b0ad0a6
RichardPeng0624 <richardpeng0624@163.com>
2023-11-27 12:37:08 +0100

    updated version of exocrires

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='exocrires',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+)# List your dependencies here
